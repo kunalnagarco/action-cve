d2f78c4a06fbf2eecb04b5c7399580d37641bbe8
Kunal Nagar <knlnagar@gmail.com>
2021-08-07 09:58:08 -0400

    wesdsd

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 /* eslint-disable no-console */
 import * as github from '@actions/github'
-// import * as core from '@actions/core'
+// import { Octokit } from '@octokit/rest'
+import * as core from '@actions/core'
 
 async function run(): Promise<void> {
-  console.log(github.context)
+  const token = core.getInput('GITHUB_TOKEN')
+  const octokit = github.getOctokit(token)
+  console.log(
+    octokit.rest.repos.checkVulnerabilityAlerts({
+      owner: '@kunalnagarco',
+      repo: 'action-cve'
+    })
+  )
   // console.log(github.context.payload.repository_vulnerability_alert)
   // const regexPattern = new RegExp(
   //   /^(?<type>build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test|¯\\_\(ツ\)_\/¯)(?<scope>\(\w+\)?((?=:\s)|(?=!:\s)))?(?<breaking>!)?(?<subject>:\s.*)?|^(?<merge>Merge \w+)/

b69577a4e9f631bc18d92609391dbd43cc61a470
Kunal Nagar <knlnagar@gmail.com>
2021-08-07 09:01:49 -0400

    qqq

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
+/* eslint-disable no-console */
 import * as github from '@actions/github'
 // import * as core from '@actions/core'
 
 async function run(): Promise<void> {
+  console.log(github.context.payload)
   console.log(github.context.payload.repository_vulnerability_alert)
   // const regexPattern = new RegExp(
   //   /^(?<type>build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test|¯\\_\(ツ\)_\/¯)(?<scope>\(\w+\)?((?=:\s)|(?=!:\s)))?(?<breaking>!)?(?<subject>:\s.*)?|^(?<merge>Merge \w+)/

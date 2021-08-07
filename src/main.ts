3343fd08bfe063b5f9ea1cd8d9f384feb1f803a3
Kunal Nagar <knlnagar@gmail.com>
2021-08-07 18:42:20 -0400

    wewewe

--- src/main.ts
+++ src/main.ts
@@ -1,56 +1,15 @@
 /* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable github/array-foreach */
 import {getOctokit} from '@actions/github'
 import {info, getInput, setFailed} from '@actions/core'
-// import { IncomingWebhook } from '@slack/webhook'
+import {IncomingWebhook} from '@slack/webhook'
 
 async function run(): Promise<void> {
   try {
     const token = getInput('token')
-    // const webhookUrl = getInput('slack_webhook')
+    const webhookUrl = getInput('slack_webhook')
     const octokit = getOctokit(token)
-    // const result = await octokit.graphql(`
-    //   query {
-    //     organization(login:"kunalnagarco") {
-    //       repository(name:"action-cve") {
-    //         vulnerabilityAlerts(first: 20) {
-    //           edges {
-    //             node {
-    //               id
-    //               securityAdvisory {
-    //                 id
-    //                 description
-    //                 cvss {
-    //                   score
-    //                   vectorString
-    //                 }
-    //                 permalink
-    //                 severity
-    //                 summary
-    //               }
-    //               securityVulnerability {
-    //                 firstPatchedVersion {
-    //                   identifier
-    //                 }
-    //                 package {
-    //                   ecosystem
-    //                   name
-    //                 }
-    //                 vulnerableVersionRange
-    //                 advisory {
-    //                   cvss {
-    //                     score
-    //                   }
-    //                   summary
-    //                 }
-    //               }
-    //             }
-    //           }
-    //         }
-    //       }
-    //     }
-    //   }
-    // `)
     const result = await octokit.graphql(`
       query {
         organization(login:"kunalnagarco") {
@@ -61,16 +20,6 @@ async function run(): Promise<void> {
                   id
                   securityAdvisory {
                     id
-                    cwes(first: 10) {
-                      edges {
-                        node {
-                          id
-                          cweId
-                          name
-                          description
-                        }
-                      }
-                    }
                     description
                     cvss {
                       score
@@ -108,11 +57,27 @@ async function run(): Promise<void> {
     if (alerts.length === 0) {
       info('No vulnerability alerts!')
     } else {
-      // const webhook = new IncomingWebhook(webhookUrl)
-      // await webhook.send({
-
-      //   text: 'Woot!'
-      // })
+      const webhook = new IncomingWebhook(webhookUrl)
+      const blocks: any = []
+      alerts.forEach((alert: any) => {
+        blocks.push({
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `
+              Package name: ${alert.node.securityVulnerability.package.name}
+              Vulnerability Version Range: ${alert.node.securityVulnerability.vulnerableVersionRange}
+              Severity: ${alert.node.securityAdvisory.severity}
+              Summary: ${alert.node.securityVulnerability.advisory.summary}
+            `
+          }
+        })
+      })
+      console.log(blocks)
+      await webhook.send({
+        blocks,
+        text: `${alerts.length} vulnerabilities found!`
+      })
       console.log(
         JSON.stringify(
           (result as any).organization.repository.vulnerabilityAlerts

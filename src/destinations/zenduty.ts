25b7af85676b898913cab2c750b5f9c31ec4974d
Kunal Nagar <knlnagar@gmail.com>
2021-12-20 10:10:08 -0500

    Change to require

--- src/destinations/zenduty.ts
+++ src/destinations/zenduty.ts
@@ -1,6 +1,9 @@
-import * as zenduty from 'zenduty-sdk'
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable import/no-commonjs */
 import { ACTION_SHORT_SUMMARY } from '../constants'
 import { Alert } from '../entities'
+const zenduty = require('zenduty-sdk')
 
 export const sendAlertsToZenduty = async (
   apiKey: string,

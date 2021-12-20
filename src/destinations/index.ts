b721e89f86a31871252d4997336b7383f8c75cfd
Kunal Nagar <knlnagar@gmail.com>
2021-12-20 12:30:57 -0500

    feat: Add destination - Zenduty (#45)

--- src/destinations/index.ts
+++ src/destinations/index.ts
@@ -1,2 +1,3 @@
 export * from './slack'
 export * from './pager-duty'
+export * from './zenduty'
---
+++ src/destinations/zenduty.ts
@@ -0,0 +1,48 @@
+import { ACTION_SHORT_SUMMARY } from '../constants'
+import { Alert } from '../entities'
+import fetch from 'node-fetch'
+
+export const sendAlertsToZenduty = async (
+  apiKey: string,
+  serviceId: string,
+  escalationPolicyId: string,
+  alerts: Alert[],
+): Promise<void> => {
+  let summary = `
+    You have ${alerts.length} vulnerabilities in ${alerts[0].repository.owner}/${alerts[0].repository.name}
+
+    ---
+
+  `
+  for (const alert of alerts) {
+    summary += `
+      Package name: ${alert.packageName}
+      Vulnerability Version Range: ${alert.vulnerability?.vulnerableVersionRange}
+      Patched Version: ${alert.vulnerability?.firstPatchedVersion}
+      Severity: ${alert.advisory?.severity}
+      Summary: ${alert.advisory?.summary}
+    `
+  }
+
+  summary += `
+
+    ---
+  `
+  const payload = {
+    service: serviceId,
+    escalation_policy: escalationPolicyId,
+    title: `${ACTION_SHORT_SUMMARY} - ${alerts[0].repository.name}`,
+    urgency: 0,
+    summary,
+  }
+  // eslint-disable-next-line i18n-text/no-en
+  const bearer = `Token ${apiKey}`
+  await fetch('https://www.zenduty.com/api/incidents/', {
+    method: 'POST',
+    headers: {
+      Authorization: bearer,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+}

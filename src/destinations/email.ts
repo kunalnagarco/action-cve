f9dda5622d544772a6a28c7bae532ffd9d2d0d41
Kunal Nagar <knlnagar@gmail.com>
2022-06-22 22:45:55 -0400

    send email table

--- src/destinations/email.ts
+++ src/destinations/email.ts
@@ -4,6 +4,40 @@ import { Alert } from '../entities'
 import { createTransport } from 'nodemailer'
 import SMTPTransport from 'nodemailer/lib/smtp-transport'
 
+const createTable = (alerts: Alert[]): string => {
+  return `
+    <table>
+      <thead>
+        <th>Package name</th>
+        <th>Vulnerability Version Range</th>
+        <th>Patched Version</th>
+        <th>Severity</th>
+        <th>Summary</th>
+        <th></th>
+      </thead>
+      <tbody>
+        ${alerts.map((alert) => {
+          createTableRow(alert)
+        })}
+      </tbody>
+    </table>
+
+  `
+}
+
+const createTableRow = (alert: Alert): string => {
+  return `
+    <tr>
+      <td>${alert.packageName}</td>
+      <td>${alert.vulnerability?.vulnerableVersionRange}</td>
+      <td>${alert.vulnerability?.firstPatchedVersion}</td>
+      <td>${alert.advisory?.severity}</td>
+      <td>${alert.advisory?.summary}</td>
+      <td><a href="${alert.advisory?.url}">View</a></td>
+    </tr>
+  `
+}
+
 export const sendAlertsToEmailSmtp = async (
   config: SMTPTransport.Options,
   alerts: Alert[],
@@ -18,6 +52,6 @@ export const sendAlertsToEmailSmtp = async (
     subject:
       subject ||
       `You have ${alerts.length} vulnerabilities in ${alerts[0].repository.owner}/${alerts[0].repository.name}`,
-    text: `Hello World!`,
+    text: createTable(alerts),
   })
 }

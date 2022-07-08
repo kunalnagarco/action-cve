f1d80630c615404450fba3a556ebb847559d4043
Kunal Nagar <2741371+kunalnagar@users.noreply.github.com>
2022-07-08 00:25:45 -0400

    Send 2 alerts with 3000

--- src/destinations/slack.ts
+++ src/destinations/slack.ts
@@ -57,7 +57,7 @@ export const sendAlertsToSlack = async (
   const webhook = new IncomingWebhook(webhookUrl)
   const alertBlocks: KnownBlock[] = []
   // for (const alert of alerts) {
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < 2; i++) {
     alertBlocks.push(createAlertBlock(alerts[i]))
   }
   await webhook.send({

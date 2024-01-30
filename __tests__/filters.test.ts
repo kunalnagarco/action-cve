04e2e8f0c89b9eb002d794f95faad7a36d336b93
Joris Buchou <joris.buchou@gmail.com>
2024-01-30 08:01:12 -0800

    Add test and improve wording

--- __tests__/filters.test.ts
+++ __tests__/filters.test.ts
@@ -1,55 +1,72 @@
-import { Alert } from "./../src/entities/alert";
-import { AdvisorySeverity } from "./../src/entities/advisory";
-import {
-  filterAlertsByAdvisorySeverity,
-} from "./../src/filters";
+import { Alert } from './../src/entities/alert'
+import { AdvisorySeverity } from './../src/entities/advisory'
+import { filterAlertsByAdvisorySeverity } from './../src/filters'
 
 const createAlert = (severity: AdvisorySeverity): Alert => ({
   repository: {
-    name: "my-package",
-    owner: "myself",
+    name: 'my-package',
+    owner: 'myself',
   },
-  packageName: "foo",
+  packageName: 'foo',
   advisory: {
     cvssScore: 4,
     severity: severity,
-    summary: "Advisory Summary",
-    description: "A description",
-    url: "https://www.foo.com",
-    publishedAt: "2020-01-01",
-    updatedAt: "2020-02-01",
-    withdrawnAt: "2020-03-01",
+    summary: 'Advisory Summary',
+    description: 'A description',
+    url: 'https://www.foo.com',
+    publishedAt: '2020-01-01',
+    updatedAt: '2020-02-01',
+    withdrawnAt: '2020-03-01',
   },
   vulnerability: undefined,
-  manifest: "abcde",
-  createdAt: "2020-01-01",
-});
+  manifest: 'abcde',
+  createdAt: '2020-01-01',
+})
 
-describe("filterAlertsByAdvisorySeverity", () => {
-  const alerts1 = createAlert("CRITICAL");
-  const alerts2 = createAlert("HIGH");
-  const alerts3 = createAlert("MODERATE");
-  const alerts4 = createAlert("LOW");
+const createAlertWithoutAdvisory = (): Alert => ({
+  repository: {
+    name: 'my-package',
+    owner: 'myself',
+  },
+  packageName: 'foo',
+  advisory: undefined,
+  vulnerability: undefined,
+  manifest: 'abcde',
+  createdAt: '2020-01-01',
+})
 
-  const alerts = [alerts1, alerts2, alerts3, alerts4];
+describe('filterAlertsByAdvisorySeverity', () => {
+  const alerts1 = createAlert('CRITICAL')
+  const alerts2 = createAlert('HIGH')
+  const alerts3 = createAlert('MODERATE')
+  const alerts4 = createAlert('LOW')
 
-  test("should filter alert by the given severity", () => {
-    const severities: AdvisorySeverity[] = ["CRITICAL", "HIGH"];
+  const alerts = [alerts1, alerts2, alerts3, alerts4]
 
-    const expected = [alerts1, alerts2];
+  test('should filter alert by the given severity', () => {
+    const severities: AdvisorySeverity[] = ['CRITICAL', 'HIGH']
 
-    expect(filterAlertsByAdvisorySeverity(alerts, severities)).toEqual(
-      expected,
-    );
-  });
+    const expected = [alerts1, alerts2]
 
-  test("should not filter alerts with an empty array of severities", () => {
-    const severities: AdvisorySeverity[] = [];
+    expect(filterAlertsByAdvisorySeverity(alerts, severities)).toEqual(expected)
+  })
 
-    const expected = alerts;
+  test('should not filter alerts with an empty array of severities', () => {
+    const severities: AdvisorySeverity[] = []
 
-    expect(filterAlertsByAdvisorySeverity(alerts, severities)).toEqual(
-      expected,
-    );
-  });
-});
+    const expected = alerts
+
+    expect(filterAlertsByAdvisorySeverity(alerts, severities)).toEqual(expected)
+  })
+
+  test("should not filter alerts when alert doesn't have severity", () => {
+    const severities: AdvisorySeverity[] = ['CRITICAL']
+    const alertsWithoutAdvisory = createAlertWithoutAdvisory()
+
+    const expected = [alertsWithoutAdvisory]
+
+    expect(
+      filterAlertsByAdvisorySeverity([alertsWithoutAdvisory], severities),
+    ).toEqual(expected)
+  })
+})
